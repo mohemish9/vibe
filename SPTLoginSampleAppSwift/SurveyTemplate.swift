bb38f05047eb7c2a580f99ffbe7773683fe50317
nsamko <ninasamko@yahoo.com>
2019-10-15 05:24:05 -0700

    Added pages 5,6,7 to the survey

--- SPTLoginSampleAppSwift/SurveyTemplate.swift
+++ SPTLoginSampleAppSwift/SurveyTemplate.swift
@@ -5,6 +5,7 @@
 //  Created by user916783 on 10/14/19.
 //  Copyright © 2019 Spotify. All rights reserved.
 //
+// Survey for the playlist generation
 
 import UIKit
 
@@ -116,7 +117,7 @@ class Page4: UIViewController {
 
     fileprivate lazy var FormalChoice = ChoiceButton(title: "Formal")
     fileprivate lazy var CasualRoomChoice = ChoiceButton(title: "Casual")
-    fileprivate lazy var NotFromalChoice = ChoiceButton(title: "Not Fromal")
+    fileprivate lazy var NotFormalChoice = ChoiceButton(title: "Not Formal")
     fileprivate lazy var BackButton = ArrowButton(title: "<")
     fileprivate lazy var NextButton = ArrowButton(title: ">")
     override func viewDidLoad() {
@@ -126,10 +127,102 @@ class Page4: UIViewController {
         view.addSubview(QuestionLabel)
         view.addSubview(FormalChoice)
         view.addSubview(CasualRoomChoice)
-        view.addSubview(NotFromalChoice)
+        view.addSubview(NotFormalChoice)
         view.addSubview(BackButton)
         view.addSubview(NextButton)
         
         
     }
 }
+
+// Asks user for the preferred genre
+class Page5: UIViewController {
+
+    fileprivate lazy var QuestionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "What is your preferred genre?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // Options for answers
+    fileprivate lazy var PopChoice = ChoiceButton(title: "Pop")
+    fileprivate lazy var RapChoice = ChoiceButton(title: "Rap")
+    fileprivate lazy var RockChoice = ChoiceButton(title: "Rock")
+    fileprivate lazy var EDMButton = ChoiceButton(title: "EDM")
+    fileprivate lazy var NoPreferenceChoice = ChoiceButton(title: "No preference")
+    fileprivate lazy var BackButton = ArrowButton(title: "<")
+    fileprivate lazy var NextButton = ArrowButton(title: ">")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(PopChoice)
+        view.addSubview(RapChoice)
+        view.addSubview(RockChoice)
+        view.addSubview(EDMButton)
+        view.addSubview(NoPreferenceChoice)
+        view.addSubview(BackButton)
+        view.addSubview(NextButton)
+        
+    }
+}
+
+// Asks user for artist preference
+class Page6: UIViewController {
+
+    fileprivate lazy var QuestionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Do you have a preferred artist?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // TODO:
+    // Give a user an option to input their preferred artist
+    fileprivate lazy var YesChoice = ChoiceButton(title: "Yes")
+    fileprivate lazy var NoChoice = ChoiceButton(title: "No")
+    fileprivate lazy var BackButton = ArrowButton(title: "<")
+    fileprivate lazy var NextButton = ArrowButton(title: ">")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(YesChoice)
+        view.addSubview(NoChoice)
+        view.addSubview(BackButton)
+        view.addSubview(NextButton)
+        
+        
+    }
+}
+
+// Asks user for playlist preference
+class Page7: UIViewController {
+
+    fileprivate lazy var QuestionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Do you want it to be similar to one of your playlists?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // TODO:
+    // How to get user's playlist to create a similar one?
+    fileprivate lazy var YesChoice = ChoiceButton(title: "Yes")
+    fileprivate lazy var NoChoice = ChoiceButton(title: "No")
+    fileprivate lazy var BackButton = ArrowButton(title: "<")
+    fileprivate lazy var DoneButton = ArrowButton(title: "DONE")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(YesChoice)
+        view.addSubview(NoChoice)
+        view.addSubview(BackButton)
+        view.addSubview(DoneButton)
+        
+        
+    }
+}
+
