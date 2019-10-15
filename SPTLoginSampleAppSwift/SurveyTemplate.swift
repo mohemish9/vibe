575ce097709c1be9745311e43ef1989c320cecef
mohemish9 <memish21@cmc.edu>
2019-10-14 20:59:09 -0700

    survey pages 2,3,4

--- SPTLoginSampleAppSwift/SurveyTemplate.swift
+++ SPTLoginSampleAppSwift/SurveyTemplate.swift
@@ -41,4 +41,95 @@ class Page1: UIViewController {
     }
 }
 
+class Page2: UIViewController {
 
+    fileprivate lazy var QuestionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "How many people are there?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+
+    fileprivate lazy var oneChoice = ChoiceButton(title: "1")
+    fileprivate lazy var twoToFourChoice = ChoiceButton(title: "2-4")
+    fileprivate lazy var fiveToTenChoice = ChoiceButton(title: "5-10")
+    fileprivate lazy var tenPlusChoice = ChoiceButton(title: "+10")
+    fileprivate lazy var BackButton = ArrowButton(title: "<")
+    fileprivate lazy var NextButton = ArrowButton(title: ">")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        
+        view.addSubview(QuestionLabel)
+        view.addSubview(oneChoice)
+        view.addSubview(twoToFourChoice)
+        view.addSubview(fiveToTenChoice)
+        view.addSubview(tenPlusChoice)
+        view.addSubview(BackButton)
+        view.addSubview(NextButton)
+        
+        
+    }
+}
+
+class Page3: UIViewController {
+
+    fileprivate lazy var QuestionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "How loud do you want it to be?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    fileprivate lazy var CalmChoice = ChoiceButton(title: "Calm")
+    fileprivate lazy var LoudRoomChoice = ChoiceButton(title: "Loud")
+    fileprivate lazy var PartyChoice = ChoiceButton(title: "Party")
+    fileprivate lazy var BackButton = ArrowButton(title: "<")
+    fileprivate lazy var NextButton = ArrowButton(title: ">")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(QuestionLabel)
+        view.addSubview(CalmChoice)
+        view.addSubview(LoudRoomChoice)
+        view.addSubview(PartyChoice)
+        view.addSubview(BackButton)
+        view.addSubview(NextButton)
+        
+        
+    }
+}
+
+
+class Page4: UIViewController {
+
+    fileprivate lazy var QuestionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "How formal do you want it to be?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+
+    fileprivate lazy var FormalChoice = ChoiceButton(title: "Formal")
+    fileprivate lazy var CasualRoomChoice = ChoiceButton(title: "Casual")
+    fileprivate lazy var NotFromalChoice = ChoiceButton(title: "Not Fromal")
+    fileprivate lazy var BackButton = ArrowButton(title: "<")
+    fileprivate lazy var NextButton = ArrowButton(title: ">")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(QuestionLabel)
+        view.addSubview(FormalChoice)
+        view.addSubview(CasualRoomChoice)
+        view.addSubview(NotFromalChoice)
+        view.addSubview(BackButton)
+        view.addSubview(NextButton)
+        
+        
+    }
+}
